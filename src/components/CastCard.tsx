b219cfd5555a791c60065bab416cea7054640b6f
CarlosMS12 <carlosmagallanes207@gmail.com>
2023-08-19 21:59:39 -0500

    add runtime & overview

---
+++ src/components/CastCard.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const CastCard = () => {
+  return (
+    <View style={styles.container}>
+      <Text>CastCard</Text>
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+    container: {}
+});
+
+export default CastCard;
--- src/screens/MovieDetailsScreen.tsx
+++ src/screens/MovieDetailsScreen.tsx
@@ -1,9 +1,12 @@
 import React, {useEffect, useState} from 'react';
-import { Text, View, StyleSheet, ScrollView, ActivityIndicator, StatusBar, ImageBackground, Image } from 'react-native';
+import { Text, View, StyleSheet, ScrollView, ActivityIndicator, StatusBar, ImageBackground, Image, FlatList } from 'react-native';
 import { baseImageUrl, movieCastDetails, movieDetails } from '../api/apicalls';
-import { COLORS, SPACING } from '../theme/theme';
+import { BORDERRADIUS, COLORS, FONTFAMILY, FONTSIZE, SPACING } from '../theme/theme';
 import AppHeader from '../components/AppHeader';
 import LinearGradient from 'react-native-linear-gradient';
+import CustomIcon from '../components/CustomIcon';
+import CategoryHeader from '../components/CategoryHeader';
+
 
 const getMovieDetails = async (movieid:number) => {
 
@@ -89,13 +92,60 @@ const MovieDetailsScreen = ({navigation,route}:any) => {
               </View>
             </LinearGradient>
           </ImageBackground>
-          <View style={styles.imageBG}>
+          <View style={styles.imageBG}></View>
             <Image
               source={{uri: baseImageUrl('w342', movieData?.poster_path)}}
               style={styles.cardImage}
             />
           </View>
-        </View>
+          <View style={styles.timeContainer}>
+            <CustomIcon name='clock' style={styles.clockIcon} />
+            <Text style={styles.runtimeText}>
+              {Math.floor(movieData?.runtime / 60)}h{' '}
+              {Math.floor(movieData?.runtime % 60)}m</Text>
+          </View>
+
+          <View>
+            <Text style={styles.title}>{movieData?.title}</Text>
+            <View style={styles.genreContainer}>
+                    {movieData?.genres.map((item: any) => {
+                        return (
+                            <View style={styles.genreBox}  key={item.id}>
+                                <Text style={styles.genreText}>{item.name}</Text>
+                            </View>
+                        );
+                    })}
+            </View>
+              <Text style={styles.tagLine}>{movieData?.tagline}</Text>
+          </View>
+
+          <View style={styles.infoContainer}>
+            <View style={styles.rateContainer}>
+              <CustomIcon name='star' style={styles.starIcon}/>
+              <Text style={styles.runtimeText}>
+                {movieData?.vote_average.toFixed(1)}{' '}({movieData?.vote_count})
+              </Text>
+              <Text style={styles.runtimeText}>
+                {movieData?.release_date.substring(8.10)}{' '}
+                {new Date(movieData?.release_date).toLocaleDateString('default', {
+                  month: 'long',
+                })}{' '}
+                {movieData?.release_date.substring(0,4)}
+              </Text>
+            </View>
+            <Text style={styles.descriptionText}>{movieData?.overview}</Text>
+          </View>
+          <View>
+            <CategoryHeader title="Top Cast"/>
+            <FlatList 
+              data={movieCastData} 
+              keyExtractor={(item:any) => item.id} 
+              horizontal 
+              contentContainerStyle={styles.containerGap24}
+              renderItem={({item, index}) => <Text>{item.original_name}</Text>}
+            />
+          </View>
+          
       </ScrollView>
     )
 }
@@ -135,6 +185,82 @@ const styles = StyleSheet.create({
       alignSelf:'center',
       bottom: 0,
       borderRadius: 20,
+    },
+    clockIcon:{
+      fontSize: FONTSIZE.size_20,
+      color: COLORS.WhiteRGBA50,
+      marginRight: SPACING.space_8,
+    },
+    timeContainer:{
+      display:  'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      paddingTop: SPACING.space_15,
+    },
+    runtimeText: {
+      fontFamily: FONTFAMILY.poppins_medium,
+      fontSize: FONTSIZE.size_14,
+      color: COLORS.White,
+    },
+    title:{
+      fontFamily: FONTFAMILY.poppins_regular,
+      fontSize: FONTSIZE.size_24,
+      color: COLORS.White,
+      marginHorizontal: SPACING.space_36,
+      marginVertical: SPACING.space_15,
+      textAlign: 'center',
+
+    },
+    genreContainer: {
+      flex: 1,
+      flexDirection: 'row',
+      gap: SPACING.space_20,
+      flexWrap: 'wrap',
+      justifyContent: 'center',
+    },
+    genreBox: {
+      borderColor: COLORS.WhiteRGBA50,
+      borderWidth:1,
+      paddingVertical: SPACING.space_4,
+      paddingHorizontal: SPACING.space_10,
+      borderRadius: BORDERRADIUS.radius_25,
+    },
+    genreText: {
+      fontFamily: FONTFAMILY.poppins_regular,
+      fontSize: FONTSIZE.size_10,
+      color: COLORS.WhiteRGBA75,
+    },
+    tagLine: {
+      fontFamily: FONTFAMILY.poppins_thin,
+      fontSize: FONTSIZE.size_14,
+      fontStyle: 'italic',
+      color: COLORS.White,
+      marginHorizontal: SPACING.space_36,
+      marginVertical: SPACING.space_15,
+      textAlign: 'center',
+    },
+    infoContainer: {
+      marginHorizontal: SPACING.space_24
+
+    },
+    rateContainer:{
+      flexDirection: 'row',
+      gap: SPACING.space_10,
+      alignItems: 'center',
+
+    },
+    starIcon:{
+      fontSize: FONTSIZE.size_20,
+      color: COLORS.Yellow,
+    },
+    descriptionText:{
+      fontFamily: FONTFAMILY.poppins_light,
+      fontSize: FONTSIZE.size_14,
+      color: COLORS.White,
+    },
+    containerGap24:{
+      gap: SPACING.space_24,
     }
 });
 
