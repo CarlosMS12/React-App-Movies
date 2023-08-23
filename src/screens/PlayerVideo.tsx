d083b8ffa0dfa9f419fce5d973370741ec6443d1
CarlosMS12 <carlosmagallanes207@gmail.com>
2023-08-23 00:33:35 -0500

    vlc player incorpored

--- src/screens/PlayerVideo.tsx
+++ src/screens/PlayerVideo.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, StyleSheet, TouchableOpacity, TouchableWithoutFeedback } from 'react-native';
 import { VLCPlayer } from 'react-native-vlc-media-player';
 import { COLORS } from '../theme/theme';
@@ -6,7 +6,7 @@ import Feather from 'react-native-vector-icons/Feather';
 
 const PlayerVideo = ({ route }: any) => {
   const { videoUrl } = route.params;
-  const [isPlaying, setIsPlaying] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(true);
   const [showControls, setShowControls] = useState(true);
   const vlcPlayerRef = useRef<any>(null);
 
@@ -36,6 +36,22 @@ const PlayerVideo = ({ route }: any) => {
     setShowControls(true);
   };
 
+  const handleJumpForward = () => {
+    vlcPlayerRef.current.seek(20); // Saltar 20 segundos hacia adelante
+  };
+
+  const handleJumpBackward = () => {
+    vlcPlayerRef.current.seek(-20); // Retroceder 20 segundos
+  };
+
+  useEffect(() => {
+    setIsPlaying(true);
+  }, []);
+
+  const initOptions = [
+    '--file-caching=15000', // Precarga de segmentos en milisegundos (20 segundos)
+  ];
+
   return (
     <TouchableWithoutFeedback onPress={handlePlayerTouch}>
       <View style={styles.container}>
@@ -47,6 +63,7 @@ const PlayerVideo = ({ route }: any) => {
           paused={!isPlaying}
           controls={true}
           playInBackground={true}
+          initOptions={initOptions}
           onPlaying={handlePlayerPlaying}
           onPaused={handlePlayerPaused}
           onStopped={handlePlayerStopped}
@@ -54,9 +71,17 @@ const PlayerVideo = ({ route }: any) => {
         />
         {showControls && (
           <>
-            <TouchableOpacity style={styles.playPauseButton} onPress={handlePlayPause}>
-              <Feather name={isPlaying ? 'pause' : 'play'} style={styles.iconStyle} />
-            </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity style={styles.jumpButton} onPress={handleJumpBackward}>
+                <Feather name="rewind" style={styles.iconStyle} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.playPauseButton} onPress={handlePlayPause}>
+                <Feather name={isPlaying ? 'pause' : 'play'} style={styles.iconStyle} />
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.jumpButton} onPress={handleJumpForward}>
+                <Feather name="fast-forward" style={styles.iconStyle} />
+              </TouchableOpacity>
+            </View>
           </>
         )}
       </View>
@@ -73,17 +98,33 @@ const styles = StyleSheet.create({
     flex: 1,
     width: '100%',
   },
-  playPauseButton: {
+  buttonContainer: {
     position: 'absolute',
     top: '50%',
-    left: '50%',
-    transform: [{ translateX: -25 }, { translateY: -25 }],
+    transform: [{ translateY: -20 }], // Ajuste para centrar verticalmente
+    left: 0,
+    right: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  playPauseButton: {
     width: 50,
     height: 50,
     borderRadius: 25,
     backgroundColor: 'rgba(0, 0, 0, 0.5)',
     justifyContent: 'center',
     alignItems: 'center',
+    marginHorizontal: 20,
+  },
+  jumpButton: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 10,
   },
   iconStyle: {
     fontSize: 24,
--- src/screens/TicketScreen.tsx
+++ src/screens/TicketScreen.tsx
@@ -1,19 +1,36 @@
-import * as React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
-
-
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import CommonVideo from '../components/VideoPlayer'; // Ajusta la ruta según la estructura de tu proyecto
 
 const TicketScreen = () => {
+  // URL del video que deseas reproducir
+  const videoUrl = 'https://streamtape.com/get_video?id=9yqXWarlzVUaawX&expires=1692830817&ip=F0uTKRgPKxSHDN&token=INqI9VSjnVss&stream=1';
+
   return (
     <View style={styles.container}>
-      <Text>HomeScreen</Text>
+      <CommonVideo
+        url={videoUrl}
+        isFull={false}
+        
+        showBack={false} // Mostrar el botón de retroceso
+        showTitle={false} // Mostrar el título
+        onEnd={() => {
+          console.log('Video terminado');
+          // Aquí puedes realizar acciones cuando el video termine de reproducirse
+        }}
+        style={{ width: '100%', height: '100%' }}
+         
+      />
     </View>
   );
 };
 
-
 const styles = StyleSheet.create({
-    container: {}
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
 });
 
 export default TicketScreen;
